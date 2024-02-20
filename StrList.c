b7c18728bcab3d00a1e683692f38e7db53910120
DanielBespalov <156921697+DanielBespalov@users.noreply.github.com>
2024-02-20 14:59:44 +0200

    Update StrList.c

--- StrList.c
+++ StrList.c
@@ -10,7 +10,7 @@ struct _StrList {
 typedef struct Node {
     char *data;
     struct Node *next;
-} Node;
+} Node_StrList;
 
 StrList* StrList_alloc() {
     StrList* list = (StrList*)malloc(sizeof(StrList));
